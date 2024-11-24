6eb0cd37f40a2188f9cfa0b83baa771a2d4a3fe9
Kleber98 <klebervieirahonorato@gmail.com>
2024-11-23 23:16:14 -0300

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,3 +1,4 @@
+gcloud config set project instabytes-442702
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
